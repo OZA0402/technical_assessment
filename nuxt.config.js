c135de6ebcb2f28c6c6af5f198160b8cb0cce5e7
OZA <za.ong@skybox.com.my>
2022-09-10 03:35:39 +0800

    install necessary packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,17 +18,19 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    "@/assets/css/main.css",
+    "ant-design-vue/dist/antd.css"
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
+  plugins: ["@/plugins/antd-ui",
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ["@nuxt/postcss8", "@nuxtjs/moment",
+  buildModules: ["@nuxt/postcss8", "@nuxtjs/moment", "@nuxtjs/fontawesome"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
---
+++ pages/marvel_characters/index.vue
@@ -0,0 +1,59 @@
+<template>
+ <div>
+   <a-card title="Default size card" style="width: 300px">
+     <a slot="extra" href="#">more</a>
+     <p>card content</p>
+     <p>card content</p>
+     <p>card content</p>
+   </a-card>
+
+   <a-card hoverable style="width: 240px">
+     <img
+       slot="cover"
+       alt="example"
+       src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+     />
+     <a-card-meta title="Europe Street beat">
+       <template slot="description">
+         www.instagram.com
+       </template>
+     </a-card-meta>
+   </a-card>
+ </div>
+</template>
+
+<script>
+import 'ant-design-vue/dist/antd.css';
+
+export default {
+  name: "index",
+  data() {
+    return{
+      results: null
+    }
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData(){
+      this.$axios.$get("https://gateway.marvel.com/v1/public/characters", {
+        params: {
+          ts: 1,
+          apikey: "78b447af6136f7b546b974a1461ecf61",
+          hash: "5ace13da8a874e4080eff29c219043c3"
+        }
+      })
+      .then((response) => {
+        console.log("response")
+        console.log(response)
+        this.results = response.data.results;
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
