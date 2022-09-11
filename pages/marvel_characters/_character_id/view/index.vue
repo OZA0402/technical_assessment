238b40c4dc88b193e97b37e799963788dbdc814b
OZA <za.ong@skybox.com.my>
2022-09-11 21:48:20 +0800

    style collapse

--- pages/marvel_characters/_character_id/view/index.vue
+++ pages/marvel_characters/_character_id/view/index.vue
@@ -22,42 +22,48 @@
           />
         </div>
 
-
-        <div class="flex flex-col text-center text-3xl">
+        <div class="flex flex-col text-center text-3xl p-4">
           {{ data.name }}
         </div>
 
         <div>
-          <a-collapse accordion>
+          <a-collapse accordion v-for="data in results" style="width: 400px">
             <a-collapse-panel key="1" header="Comics">
-              <div v-for="data in results">
-                <p v-for="comic in data.comics.items">
-                  {{comic.name}}
-                </p>
-              </div>
+
+                <a-list :data-source="data.comics.items">
+                  <a-list-item slot="renderItem" slot-scope="item, index" @click="comicModal(item, index)">
+                    {{ item.name }}
+                  </a-list-item>
+                </a-list>
+
             </a-collapse-panel>
             <a-collapse-panel key="2" header="Series">
-              <div v-for="data in results">
-                <p v-for="comic in data.series.items">
-                  {{comic.name}}
-                </p>
-              </div>
+
+                <a-list :data-source="data.series.items">
+                  <a-list-item slot="renderItem" slot-scope="item, index" >
+                    {{ item.name }}
+                  </a-list-item>
+                </a-list>
+
             </a-collapse-panel>
             <a-collapse-panel key="3" header="Stories">
-              <div v-for="data in results">
-                <p v-for="comic in data.stories.items">
-                  {{comic.name}}
-                </p>
-              </div>
+
+                <a-list :data-source="data.stories.items">
+                  <a-list-item slot="renderItem" slot-scope="item, index">
+                    {{ item.name }}
+                  </a-list-item>
+                </a-list>
+
             </a-collapse-panel>
             <a-collapse-panel key="4" header="Events">
-              <div v-for="data in results">
-                <p v-for="comic in data.events.items">
-                  {{comic.name}}
-                </p>
-              </div>
-            </a-collapse-panel>
 
+                <a-list :data-source="data.events.items">
+                  <a-list-item slot="renderItem" slot-scope="item, index">
+                    {{ item.name }}
+                  </a-list-item>
+                </a-list>
+
+            </a-collapse-panel>
           </a-collapse>
         </div>
 
@@ -107,6 +113,15 @@ export default {
           this.results = response.data.results;
         });
     },
+
+    comicModal(data, index){
+      console.log("data")
+      console.log(data)
+      console.log("index")
+      console.log(index)
+      
+    }
+
   },
 };
 </script>
